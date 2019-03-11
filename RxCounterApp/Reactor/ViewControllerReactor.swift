502d6ae2a11fdaf7063d87a895cee019269ea2bf
Ken Phanith <phanith.ken@quad.co.jp>
2019-03-11 23:02:24 +0900

    init

--- RxCounterApp/Reactor/ViewControllerReactor.swift
+++ RxCounterApp/Reactor/ViewControllerReactor.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import RxSwift
+import ReactorKit
+
+class ViewControllerReactor: Reactor {
+    
+    enum Action {
+        case increment
+        case decrement
+    }
+    
+    enum Mutation {
+        case increment
+        case decrement
+    }
+    
+    struct State {
+        var counter: Int
+    }
+    
+    var initialState: ViewControllerReactor.State = State(
+        counter: 0
+    )
+    
+    init() {
+        
+    }
+    
+    func mutate(action: ViewControllerReactor.Action) -> Observable<ViewControllerReactor.Mutation> {
+        switch action {
+            
+        case .increment:
+            return Observable.just(Mutation.increment)
+            
+        case .decrement:
+            return Observable.just(Mutation.decrement)
+            
+        }
+    }
+    
+    func reduce(state: ViewControllerReactor.State, mutation: ViewControllerReactor.Mutation) -> ViewControllerReactor.State {
+        
+        var newState = state
+        switch mutation {
+            
+        case .increment:
+            newState.counter += 1
+            break
+        
+        case .decrement:
+            newState.counter -= 1
+            break
+        }
+        return newState
+    }
+}
--- RxCounterApp/ViewController.swift
+++ RxCounterApp/ViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
+import RxSwift
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+class ViewController: UIViewController, View {
+    
+    typealias Reactor = ViewControllerReactor
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    /// create UI elm
+    let incrementBtn: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+    let decrementBtn: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+    let counterLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: 20, height: 30))
 
 }
 
+
+// MARK: - Reactor
+extension ViewController {
+    
+    func bind(reactor: ViewControllerReactor) {
+        
+        self.incrementBtn.rx.tap
+            .map { Reactor.Action.increment }
+            .bind(to: reactor.action)
+            .disposed(by: self.disposeBag)
+        
+        self.decrementBtn.rx.tap
+            .map { Reactor.Action.decrement }
+            .bind(to: reactor.action)
+            .disposed(by: self.disposeBag)
+        
+        self.reactor?.state
+            .map { String($0.counter) }
+            .bind(to: self.counterLabel.rx.text)
+            .disposed(by: self.disposeBag)
+        
+    }
+    
+}
+
+
+// MARK: - LIFECYCLE
+extension ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // positioning
+        self.incrementBtn.center = CGPoint(x: (self.view.frame.size.width / 2) + 50, y: self.view.frame.size.height / 2)
+        self.decrementBtn.center = CGPoint(x: (self.view.frame.size.width / 2) - 50, y: self.view.frame.size.height / 2)
+        self.counterLabel.center = CGPoint(x: self.view.frame.size.width / 2, y: self.view.frame.size.height / 2)
+        
+        /// set btn title
+        self.incrementBtn.setTitle("+", for: .normal)
+        self.decrementBtn.setTitle("-", for: .normal)
+        self.counterLabel.text = "0"
+        
+        /// set btn background
+        self.incrementBtn.backgroundColor = UIColor.gray
+        self.decrementBtn.backgroundColor = UIColor.gray
+        
+        /// add subviews
+        self.view.addSubview(incrementBtn)
+        self.view.addSubview(decrementBtn)
+        self.view.addSubview(counterLabel)
+        
+        self.reactor = ViewControllerReactor()
+    }
+    
+}
